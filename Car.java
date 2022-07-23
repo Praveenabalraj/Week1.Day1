37449c4c3069761d3c0b65c64e47d7350372a410
Praveenabalraj <109019674+Praveenabalraj@users.noreply.github.com>
2022-07-23 10:25:32 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Car {
+	byte wheel = 4;
+	short speed = 32000;
+	int regNum = 111111111;
+	long chasisNumber = 12345678910L;
+	float bankBalance = 15123.10f;
+	double kiloMeters = 20043.12d;
+	char model = 'A';
+	boolean airbag = true;
+	String name = "TESTLEAF";
+
+	public static void main(String[] args) {
+		System.out.println("wheel");
+	}
+}
---
+++ Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+	
+	float version = 100.2f;
+	String name = "firefox";
+	boolean visible = true;
+	int year = 1998;
+	char logo = 'M';
+	public static void main(String[] args) {
+	    System.out.println("This is my first Script");
+		System.out.println("version");
+		System.out.println("name");
+		System.out.println("visible");
+		System.out.println("year");
+		System.out.println("logo");
+
+	}
+
+}
